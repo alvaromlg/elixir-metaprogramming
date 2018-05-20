c96a1be676852c6cca62ad644206d7decb9c075b
Alvaro <apelegrinafernandez@gmail.com>
2018-05-20 20:23:35 +0200

    dynamic lang loader functions

---
+++ lib/lang_loader.ex
@@ -0,0 +1,30 @@
+defmodule ElixirMeta.LangLoader do
+  @moduledoc """
+  Dynamically generates lang functions depending on
+  lang files available
+  i.e:
+  lang("es")
+  lang_es
+  ...
+  """
+
+  @external_resource [Path.join([__DIR__, "es.json"]),
+                      Path.join([__DIR__, "en.json"])]
+
+  defmacro __using__(_) do
+    for lang <- ["es", "en"] do
+      {:ok, body} = File.read(Path.join([__DIR__, "#{lang}.json"]))
+      {:ok, json} = Poison.decode(body)
+      quote do
+        # Lang.lang("es")
+        def lang(unquote(lang)), do: unquote(Macro.escape(json))
+        # Lang.lang_es
+        def unquote(:"lang_#{lang}")(), do: unquote(Macro.escape(json))
+      end
+    end
+  end
+end
+
+defmodule ElixirMeta.Lang do
+  use ElixirMeta.LangLoader
+end
